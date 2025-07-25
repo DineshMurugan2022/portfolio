b914010aa49e36e8a5593c6d63296a1213be7274
DineshMurugan2022 <2002dineshmurugan@gmail.com>
2025-07-25 17:38:24 +0530

    5454

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Make sure build output goes here
+  },
+  server: {
+    port: 3000,      // Optional: dev server port
+  }
 })
